9f5617771230e829ec8d9178a7d050dc6b81844b
mobina jafari <mobinajafari.dev@gmail.com>
2026-05-09 19:55:11 +0330

    model class for extending models

--- src/Core/Model.php
+++ src/Core/Model.php
@@ -6,24 +6,41 @@ use Helpers\Database;
 abstract class Model {
     protected $table;
     protected $db;
-    
+
     public function __construct() {
-        $this->db = Database::getConnection();
+        $config = require __DIR__ . '/../config/database.php';
+        $this->db = new Database($config);
     }
-    
-    public function query($sql, $params = []) {
-        return Database::query($sql, $params);
+
+    public function insert($data, $lastID = true) {
+        return $this->db->insert($this->table, $data, $lastID);
     }
-    
-    public function queryAll($sql, $params = []) {
-        return Database::queryAll($sql, $params);
+
+    public function select($fields = ['*'], $where = [], $fetchAll = true, $params = '') {
+        return $this->db->select($this->table, $fields, $where, $fetchAll, $params);
     }
-    
-    public function execute($sql, $params = []) {
-        return Database::execute($sql, $params);
+
+    public function update($data, $where) {
+        return $this->db->update($this->table, $data, $where);
     }
-    
-    public function lastInsertId() {
-        return Database::lastInsertId();
+
+    public function delete($where) {
+        return $this->db->delete($this->table, $where);
+    }
+
+    public function rawQuery($sql, $params = []) {
+        return $this->db->query($sql, $params);
+    }
+
+    public function beginTransaction() {
+        return $this->db->beginTransaction();
+    }
+
+    public function commit() {
+        return $this->db->commit();
+    }
+
+    public function rollBack() {
+        return $this->db->rollBack();
     }
 }

5c21a91151258fe2900d8a8ffcb74a06c39eb0ef
mobina jafari <mobinajafari.dev@gmail.com>
2026-05-12 10:26:24 +0330

    name , user corrected

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'host' => getenv('DB_HOST'),
-    'dbname' => getenv('DB_NAME'),
-    'user' => getenv('DB_USER'),
+    'name' => getenv('DB_NAME'),
+    'user' => getenv('DB_USER') ,
     'password' => getenv('DB_PASS'),
     'charset' => 'utf8mb4'
 ];
